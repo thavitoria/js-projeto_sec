a77720e597dfa046046b8fbe5dc3ae6483a5f4df
thavitoria <thalitavitoriaescola@gmail.com>
2025-03-20 21:55:44 -0300

    UPDATE

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
-<<<<<<< HEAD
-let titulio = document.querySelector('h1');
-titulio.innerHTML = 'Oi Mundo';
-
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo de Adivinhação';
+ 
 let subtitulo = document.querySelector('p');
-subtitulo.innerHTML = 'escolha um numero entre 1 e 100';
-
-function verificarChute() {
-    alert('apertou o botão');
-}
-=======
-// let titulo = document.querySelector('h1');
-// titulo.innerHTML = 'Jogo de adivinhação';
-
-// let subtitulo = document.querySelector('p');
-// subtitulo.innerHTML = 'Escolha um número entre 1 e 100';
-
+subtitulo.innerHTML = ' Escolha um número de 1 e 100';
+ 
 exibirTextoNaTela('h1', 'Jogo de adivinhação');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
-
+ 
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 100) + 1;
+}
+ 
+let numeroSecreto = gerarNumeroAleatorio();
+console.log (numeroSecreto);  
+ 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
-
+ 
 function verificarChute() {
-    console.log('Apertou o botão');
+    let chute = parseInt (document.querySelector('input').value);
+    console.log(chute  === numeroSecreto);
 }
-
-//<h1>Jogo de adivinhação</h1>
-//<p>Escolha um número entre 1 e 100</p>
->>>>>>> ed78fcbe0f71bbd87459d9d06682a62e04fd8bfb
