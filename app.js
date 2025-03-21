b4fb042ec60a95a4ce020992032d93f7ce93a883
thavitoria <thalitavitoriaescola@gmail.com>
2025-03-20 21:09:45 -0300

    primeira logica

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulio = document.querySelector('h1');
+titulio.innerHTML = 'Oi Mundo';
+
+let subtitulo = document.querySelector('p');
+subtitulo.innerHTML = 'escolha um numero entre 1 e 100';
+
+function verificarChute() {
+    alert('apertou o botão');
+}
